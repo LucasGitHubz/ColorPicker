fc0b3bfe56c04816dfb817d2351908b947abd0fe
Lucas <lucas.amelin@anybuddyapp.com>
2021-03-26 11:29:19 +0100

    DialogFragment setup

---
+++ ColorPicker/src/main/java/com/example/colorpicker/ColorControllerFragment.java
@@ -0,0 +1,79 @@
+package com.example.colorpicker;
+
+import android.app.Activity;
+import android.app.DialogFragment;
+import android.content.ContextWrapper;
+import android.content.res.ColorStateList;
+import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+public class ColorControllerFragment extends DialogFragment {
+    FloatingActionButton floatingActionButton;
+    Integer color = -1111111;
+    ImageView rgbWheel;
+    Button confirmColorButton;
+    Activity mActivity;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.color_picker_fragment, container, false);
+        configureLayout(v);
+
+        return v;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        ViewGroup.LayoutParams params = getDialog().getWindow().getAttributes();
+        params.width = ViewGroup.LayoutParams.MATCH_PARENT;
+        getDialog().getWindow().setAttributes((android.view.WindowManager.LayoutParams) params);
+    }
+
+    void configureLayout(View v) {
+        rgbWheel = v.findViewById(R.id.rgbWheel);
+        confirmColorButton = v.findViewById(R.id.confirmColorBtn);
+
+        rgbWheel.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                Bitmap bitmap;
+                if (event.getAction() == MotionEvent.ACTION_DOWN || event.getAction() == MotionEvent.ACTION_MOVE) {
+                    bitmap = rgbWheel.getDrawingCache();
+
+                    int pixel = bitmap.getPixel((int)event.getX(), (int)event.getY());
+
+                    int r = Color.red(pixel);
+                    int g = Color.green(pixel);
+                    int b = Color.blue(pixel);
+
+                    color = Color.rgb(r, g ,b);
+                    confirmColorButton.setBackgroundColor(color);
+                }
+                return true;
+            }
+        });
+
+        confirmColorButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                floatingActionButton.setBackgroundTintList(ColorStateList.valueOf(color));
+                dismiss();
+            }
+        });
+    }
+}
--- ColorPicker/src/main/java/com/example/colorpicker/ColorPicker.java
+++ ColorPicker/src/main/java/com/example/colorpicker/ColorPicker.java
@@ -1,33 +1,48 @@
 package com.example.colorpicker;
 
+import android.app.Activity;
+import android.app.DialogFragment;
+import android.app.Fragment;
+import android.app.FragmentTransaction;
+import android.content.Context;
+import android.content.ContextWrapper;
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.Color;
+import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-import java.util.List;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 
-public class ColorPicker {
-    public static void fetch(View view1, View viewsToPassColor, View secondView) {
-        view1.setOnTouchListener(new View.OnTouchListener() {
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+public class ColorPicker extends AppCompatActivity {
+    public void launch(View viewLauncher, FloatingActionButton floatingActionButton) {
+        viewLauncher.setOnClickListener(new View.OnClickListener() {
             @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                Bitmap bitmap;
-                if (event.getAction() == MotionEvent.ACTION_DOWN || event.getAction() == MotionEvent.ACTION_MOVE) {
-                    bitmap = view1.getDrawingCache();
-
-                    int pixel = bitmap.getPixel((int)event.getX(), (int)event.getY());
-
-                    int r = Color.red(pixel);
-                    int g = Color.green(pixel);
-                    int b = Color.blue(pixel);
-
-                    Integer color = Color.rgb(r, g ,b);
-                    viewsToPassColor.setBackgroundColor(color);
-                    secondView.setBackgroundColor(color);
-                }
-                return true;
+            public void onClick(View v) {
+                fragmentLauncher(floatingActionButton);
             }
         });
+    }
+    void fragmentLauncher(FloatingActionButton floatingActionButton) {
+        FragmentTransaction ft = getFragmentManager().beginTransaction();
+        Fragment prev = getFragmentManager().findFragmentByTag("dialog");
+        if (prev != null) {
+            ft.remove(prev);
+        }
+        ft.addToBackStack(null);
+        ColorControllerFragment test = new ColorControllerFragment();
+        test.floatingActionButton = floatingActionButton;
+        DialogFragment dialogFragment = test;
+        dialogFragment.show(ft, "dialog");
     }
 }
