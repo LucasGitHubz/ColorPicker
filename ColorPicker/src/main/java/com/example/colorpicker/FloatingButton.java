8ad25813217498a2decd9130daa7af4a64b8fbda
gcattan <gcattan@hotmail.fr>
2022-01-04 15:00:08 +0100

    missing import

--- ColorPicker/src/main/java/com/example/colorpicker/FloatingButton.java
+++ ColorPicker/src/main/java/com/example/colorpicker/FloatingButton.java
@@ -6,6 +6,7 @@ import android.app.Fragment;
 import android.app.FragmentTransaction;
 import android.content.Context;
 import android.content.res.ColorStateList;
+import android.graphics.Color;
 import android.util.AttributeSet;
 import android.view.View;
 
